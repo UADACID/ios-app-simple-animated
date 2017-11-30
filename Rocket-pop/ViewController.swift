57a92661f9c08e760c685ce565fa090b3d6aaa22
Pratama <pratamasetya99@gmail.com>
2017-12-01 00:34:26 +0700

    Initial Commit

---
+++ Rocket-pop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rocket-pop
+//
+//  Created by pratama setya aji on 12/1/17.
+//  Copyright © 2017 pratama setya aji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
